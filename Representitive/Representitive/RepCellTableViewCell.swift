617b975b8dc81475b9295c54b34f16ac4c782daa
Justin Carver <15Jcarver202@gmail.com>
2016-08-29 16:36:11 -0600

    Added all files that may be needed. Also changed the appDelegate to not include CoreData

---
+++ Representitive/Representitive/RepCellTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  RepCellTableViewCell.swift
+//  Representitive
+//
+//  Created by Justin Carver on 8/29/16.
+//  Copyright © 2016 Justin Carver. All rights reserved.
+//
+
+import UIKit
+
+class RepCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
---
+++ Representitive/Representitive/StateController.swift
@@ -0,0 +1,9 @@
+//
+//  StateController.swift
+//  Representitive
+//
+//  Created by Justin Carver on 8/29/16.
+//  Copyright © 2016 Justin Carver. All rights reserved.
+//
+
+import Foundation
